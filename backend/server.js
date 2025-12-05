6bb2cc49ead37cf240af3327d3122e494e2e22d4
Provash-roy <provashray94@gmail.com>
2025-12-05 23:40:42 +0600

    After Added DB

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,22 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World! Provash");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+connectDb();
